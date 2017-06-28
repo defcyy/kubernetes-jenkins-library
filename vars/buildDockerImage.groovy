c7415709511aec6d23fc2caa14649118d92121a7
CaoYY <defcyy@gmail.com>
2017-06-28 22:21:56 +0800

    fix build docker image function

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -4,9 +4,9 @@ def call(String service, String version, String path, String credentialId) {
     def common = new org.iti.Common()
 
     def image = common.dockerImage(service, version)
-
     sh "docker build -t ${image} ${path}"
-    utils.dockerLogin(common.projectConfig.docker_registry, credentialId)
+
+    common.dockerLogin(credentialId)
     sh "docker push ${image}"
 }
 
